c5db19a67e0be2c9f8af467ee5620257910b3c26
Afshin Arani <afshin@arani.dev>
2021-11-19 21:08:37 +0330

    scripts: move snap release from GitHubCI to GitLabCI
    The reason to use SNAPCRAFT_LOGIN_FILE instead
    of SNAPCRAFT_LOGIN (like in Github) is in the docs:
    
    From https://docs.gitlab.com/ee/ci/variables/#cicd-variable-types :
    
    ...
    File type variables:
    
    Consist of a key, value and file.
    Are made available in jobs as environment variables, with
    The CI/CD variable key as the environment variable name.
    The CI/CD variable value saved to a temporary file.
    The path to the temporary file as the environment variable value.
    ...
    
    And the reason for using "needs:" instead of "dependencies:"
    is because the job depends on artifacts of a job in the same
    stage. More info: https://gitlab.com/gitlab-org/gitlab/-/issues/30632
    
    Co-authored-by: Andres G. Aragoneses <knocte@gmail.com>

--- scripts/snap_release.fsx
+++ scripts/snap_release.fsx
@@ -24,6 +24,15 @@ open Process
 #load "fsxHelper.fs"
 open GWallet.Scripting
 
+#r "System.Net.Http.dll"
+#r "System.Web.Extensions.dll"
+open System.Web.Script.Serialization
+#load "githubCiCheck.fs"
+open GWallet.Github
+
+let commitHash = Git.GetLastCommit()
+let currentBranch = Git.GetCurrentBranch()
+
 let snapFiles = FsxHelper.RootDir.EnumerateFiles().Where(fun file -> file.Name.EndsWith ".snap")
 if not (snapFiles.Any()) then
     Console.Error.WriteLine "No snap package found."
@@ -34,38 +43,69 @@ if null = snapFile then
     Console.Error.WriteLine "Too many snap packages found, please discard invalid/old ones first."
     Environment.Exit 2
 
-Console.WriteLine "Checking if this is a tag commit..."
+
 let githubRef = Environment.GetEnvironmentVariable "GITHUB_REF"
-if String.IsNullOrEmpty githubRef then
-    failwith "GITHUB_REF var not found. Beware: manual logging for release has been disabled, only automated CI jobs can upload now"
-
+let gitlabRef = Environment.GetEnvironmentVariable "CI_COMMIT_REF_SLUG"
 let tagsPrefix = "refs/tags/"
-if not (githubRef.StartsWith tagsPrefix) then
-    Console.WriteLine (sprintf "No tag being set (GITHUB_REF=%s), skipping release." githubRef)
-    Environment.Exit 0
 
-let gitTag = githubRef.Substring tagsPrefix.Length
-if not (snapFile.FullName.Contains gitTag) then
-    Console.Error.WriteLine (
-        sprintf "Git tag (%s) doesn't match version in snap package file name (%s)"
-            gitTag
-            snapFile.FullName
-    )
-    Environment.Exit 3
+Console.WriteLine "Checking if this is a tag commit..."
+if not (String.IsNullOrEmpty githubRef) then
+    if not (githubRef.StartsWith tagsPrefix) then
+        Console.WriteLine (sprintf "No tag being set (GITHUB_REF=%s), skipping release." githubRef)
+        Environment.Exit 0
 
-Console.WriteLine (sprintf "About to start upload of release %s" gitTag)
+    let gitTag = githubRef.Substring tagsPrefix.Length
+    if not (snapFile.FullName.Contains gitTag) then
+        Console.Error.WriteLine (
+            sprintf "Git tag (%s) doesn't match version in snap package file name (%s)"
+                gitTag
+                snapFile.FullName
+        )
+        Environment.Exit 3
 
-let snapcraftLoginFileName = Path.Combine(FsxHelper.RootDir.FullName, "snapcraft.login")
-if File.Exists snapcraftLoginFileName then
-    Console.WriteLine "snapcraft.login file found, skipping log-in"
-else
-    let snapcraftLogin = Environment.GetEnvironmentVariable "SNAPCRAFT_LOGIN"
-    if String.IsNullOrEmpty snapcraftLogin then
-        failwith "Manual logging for release has been disabled, only automated CI jobs can upload now"
+    Console.WriteLine (sprintf "About to start upload of release %s" gitTag)
+
+    let snapcraftLoginFileName = Path.Combine(FsxHelper.RootDir.FullName, "snapcraft.login")
+    if File.Exists snapcraftLoginFileName then
+        Console.WriteLine "snapcraft.login file found, skipping log-in"
     else
-        Console.WriteLine "Automatic login about to begin..."
-        File.WriteAllText(snapcraftLoginFileName, snapcraftLogin)
+        let snapcraftLogin = Environment.GetEnvironmentVariable "SNAPCRAFT_LOGIN"
+        if String.IsNullOrEmpty snapcraftLogin then
+            failwith "Manual logging for release has been disabled, only automated CI jobs can upload now"
+        else
+            Console.WriteLine "Automatic login about to begin..."
+            File.WriteAllText(snapcraftLoginFileName, snapcraftLogin)
+elif not (String.IsNullOrEmpty gitlabRef) then
+    if not (gitlabRef.StartsWith tagsPrefix) then
+        Console.WriteLine (sprintf "No tag being set (CI_COMMIT_REF_SLUG=%s), skipping release." gitlabRef)
+        Environment.Exit 0
 
+    let gitTag = gitlabRef.Substring tagsPrefix.Length
+
+    if not (snapFile.FullName.Contains gitTag) then
+        Console.Error.WriteLine (
+            sprintf "Git tag (%s) doesn't match version in snap package file name (%s)"
+                gitTag
+                snapFile.FullName
+        )
+        Environment.Exit 3
+
+    Console.WriteLine (sprintf "About to start upload of release %s" gitTag)
+
+    GithubActions.MakeSureGithubCIPassed commitHash currentBranch
+
+    let snapcraftLoginFileName = Path.Combine(FsxHelper.RootDir.FullName, "snapcraft.login")
+    if File.Exists snapcraftLoginFileName then
+        Console.WriteLine "snapcraft.login file found, skipping log-in"
+    else
+        let snapcraftLoginTempAddress = Environment.GetEnvironmentVariable "SNAPCRAFT_LOGIN_FILE"
+        if String.IsNullOrEmpty snapcraftLoginTempAddress then
+            failwith "SNAPCRAFT_LOGIN_FILE secret doesn't exist, can't release."
+
+        File.Copy(snapcraftLoginTempAddress, snapcraftLoginFileName)
+else
+    failwith "No github or gitlab variable found, beware: manual logging for release has been disabled, only automated CI jobs can upload now"
+    
 // if this fails, use `snapcraft export-login` to generate a new token
 Process.SafeExecute ({ Command = "snapcraft"; Arguments = "login --with snapcraft.login" }, Echo.All)
 |> ignore
