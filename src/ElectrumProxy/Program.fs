452de7ed186ab181f0e5b56a4b9f64d643fee57b
webwarrior <reg@webwarrior.ws>
2024-09-10 13:10:12 +0200

    ElectrumProxy: refactoring
    Created Server module and moved some code there.

--- src/ElectrumProxy/Program.fs
+++ src/ElectrumProxy/Program.fs
@@ -1,23 +1,10 @@
 ﻿module Program
 
-open System
-open System.Text
 open System.Net.Sockets
 
 open StreamJsonRpc
 
-let supportedProtocolVersion = "0.10"
 
-type PascalCaseToSnakeCaseNamingPolicy() = 
-    inherit Json.JsonNamingPolicy()
-
-    static let capitalizedWordRegex = RegularExpressions.Regex "[A-Z][a-z0-9]*"
-
-    override self.ConvertName name =
-        let evaluator (regexMatch: RegularExpressions.Match) =
-            let lowercase = regexMatch.Value.ToLower()
-            if regexMatch.Index = 0 then lowercase else "_" + lowercase
-        capitalizedWordRegex.Replace(name, Text.RegularExpressions.MatchEvaluator evaluator)
 
 [<EntryPoint>]
 let main (args: string[]) =
@@ -31,15 +18,13 @@ let main (args: string[]) =
 
     use formatter = new SystemTextJsonFormatter()
     use handler = new NewLineDelimitedMessageHandler(networkStream, networkStream, formatter)
-    formatter.JsonSerializerOptions.PropertyNamingPolicy <- PascalCaseToSnakeCaseNamingPolicy()
+    formatter.JsonSerializerOptions.PropertyNamingPolicy <- Server.PascalCaseToSnakeCaseNamingPolicy()
 
     async {
         while true do
             use jsonRpc = new JsonRpc(handler)
             
-            jsonRpc.AddLocalRpcMethod(
-                "server.version", 
-                new Func<string, string, string>(fun _clientVersion _protocolVersion -> supportedProtocolVersion))
+            Server.AddMethods jsonRpc
 
             jsonRpc.StartListening()
             do! jsonRpc.Completion |> Async.AwaitTask
