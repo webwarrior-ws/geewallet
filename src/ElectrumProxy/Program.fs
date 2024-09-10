e998d6d5d30b8a8fc9e3b8e3ddd04af47e135c15
webwarrior <reg@webwarrior.ws>
2024-09-10 15:15:31 +0200

    ElectrumProxy: work on general server logic
    Currently Liana fails to establish a connection.

--- src/ElectrumProxy/Program.fs
+++ src/ElectrumProxy/Program.fs
@@ -5,26 +5,28 @@ open System.Net.Sockets
 open StreamJsonRpc
 
 
-
 [<EntryPoint>]
 let main (args: string[]) =
     let port = int args.[0]
 
     let listener = new TcpListener(System.Net.IPAddress.Any, port)
     listener.Start();
-    
-    use tcpClient = listener.AcceptTcpClient();
-    use networkStream = tcpClient.GetStream()
-
-    use formatter = new SystemTextJsonFormatter()
-    use handler = new NewLineDelimitedMessageHandler(networkStream, networkStream, formatter)
-    formatter.JsonSerializerOptions.PropertyNamingPolicy <- Server.PascalCaseToSnakeCaseNamingPolicy()
 
     async {
         while true do
+            use! tcpClient = listener.AcceptTcpClientAsync() |> Async.AwaitTask
+            use networkStream = tcpClient.GetStream()
+
+            use formatter = new SystemTextJsonFormatter()
+            use handler = new NewLineDelimitedMessageHandler(networkStream, networkStream, formatter)
+            formatter.JsonSerializerOptions.PropertyNamingPolicy <- Server.PascalCaseToSnakeCaseNamingPolicy()
+
             use jsonRpc = new JsonRpc(handler)
             
             Server.AddMethods jsonRpc
+
+            jsonRpc.Disconnected.Add(fun args -> 
+                eprintfn "Disconnected. Reason=%A; Description=%A; Exception=%A" args.Reason args.Description args.Exception)
 
             jsonRpc.StartListening()
             do! jsonRpc.Completion |> Async.AwaitTask
